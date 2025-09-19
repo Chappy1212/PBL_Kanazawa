07b82ecd3552acb25fbe569ec250b75144dae5d3
Sota Ura <208552856+Chappy1212@users.noreply.github.com>
2025-09-19 16:03:12 +0900

    Delete PBL_Kanazawa-main directory

--- PBL_Kanazawa-main/spec/dummy.spec.ts
+++
@@ -1,5 +0,0 @@
-describe('dummy spec', () => {
-  it('should pass', () => {
-    expect(true).toBe(true);
-  });
-});
--- PBL_Kanazawa-main/src/db/connection-pool.ts
+++
@@ -1,7 +0,0 @@
-import { Pool } from 'pg';
-import { dbConfig } from './config';
-
-export const connectionPool = new Pool({
-  ...dbConfig,
-  max: 10,
-});
